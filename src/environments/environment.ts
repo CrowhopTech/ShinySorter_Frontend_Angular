d25b7c9c8b858c131430b0b2715bd938af5193f2
Adam Krpan <akrpan@crowhop.tech>
2022-11-29 22:36:37 -0800

    Transfer commit of supabase work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://pmtnfgtrjfxzgtspqnmk.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdG5mZ3RyamZ4emd0c3Bxbm1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk0MzM5MTQsImV4cCI6MTk4NTAwOTkxNH0.TdA8yO2sZ9NOqKMKolo7mFSZEBiKbI6wbdFfv5_KTMI',
 };
 
 /*
