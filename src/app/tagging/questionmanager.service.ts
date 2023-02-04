b01d3530e5d4086f25330f16f5c05aa3ec91c129
Adam Krpan <adam.krpan@outlook.com>
2023-02-04 00:11:39 -0800

    Fixed the tagging page eating all navigation events
    Closes #21 and closes #19

--- src/app/tagging/questionmanager.service.ts
+++ src/app/tagging/questionmanager.service.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Question, QuestionWithOptions, SupabaseService, TaggedFileEntry } from '../supabase.service';
 
-const imageParam = "image"
-const selectedTagsParam = "selectedTags"
-const orderingIDParam = "orderingID"
+const imageParam = "image";
+const selectedTagsParam = "selectedTags";
+const orderingIDParam = "orderingID";
 
 // Responsible for:
 // * Managing which question we're on (query params, navigating to next question, etc.)
@@ -15,39 +15,39 @@ const orderingIDParam = "orderingID"
   providedIn: 'root'
 })
 export class QuestionManagerService {
-  private _querySubscription?: Subscription = undefined
+  private _querySubscription?: Subscription = undefined;
 
-  private _currentFileID?: number = undefined
-  public get currentFileID() { return this._currentFileID }
-  private _currentFile?: TaggedFileEntry = undefined
-  public get currentFile() { return this._currentFile }
+  private _currentFileID?: number = undefined;
+  public get currentFileID() { return this._currentFileID; }
+  private _currentFile?: TaggedFileEntry = undefined;
+  public get currentFile() { return this._currentFile; }
 
-  private _selectedTags: number[] = []
-  public get selectedTags() { return this._selectedTags }
+  private _selectedTags: number[] = [];
+  public get selectedTags() { return this._selectedTags; }
 
-  private _completionPercentage: number = 0
-  public get completionPercentage() { return this._completionPercentage }
+  private _completionPercentage: number = 0;
+  public get completionPercentage() { return this._completionPercentage; }
 
-  private _fetchErr?: string
-  public get fetchErr() { return this._fetchErr }
+  private _fetchErr?: string;
+  public get fetchErr() { return this._fetchErr; }
 
-  private _questions?: QuestionWithOptions[] = undefined
-  private _orderingID?: number = undefined
-  private _currentQuestion: QuestionWithOptions | null | undefined = undefined // undefined means hasn't loaded yet, null means we're done (no more questions), Question means current question
-  public get currentQuestion() { return this._currentQuestion }
+  private _questions?: QuestionWithOptions[] = undefined;
+  private _orderingID?: number = undefined;
+  private _currentQuestion: QuestionWithOptions | null | undefined = undefined; // undefined means hasn't loaded yet, null means we're done (no more questions), Question means current question
+  public get currentQuestion() { return this._currentQuestion; }
 
   constructor(private router: Router, private route: ActivatedRoute, private supaService: SupabaseService) {
   }
 
   private getNumberArrayParam(params: Params, param: string): number[] {
-    const val: string = params[param]
+    const val: string = params[param];
     if (!val) {
-      return []
+      return [];
     }
 
-    const split = val.split(",")
-    const parsed = split.map(s => parseInt(s))
-    return parsed
+    const split = val.split(",");
+    const parsed = split.map(s => parseInt(s));
+    return parsed;
   }
 
   private renavigate() {
@@ -59,141 +59,145 @@ export class QuestionManagerService {
           [selectedTagsParam]: this._selectedTags.join(","),
         }
       }
-    )
+    );
   }
 
   private wipeVars() {
     // Clear any stale data from past file
-    this._currentFileID = undefined
-    this._currentFile = undefined
-    this._selectedTags = []
-    this._questions = undefined
-    this._currentQuestion = undefined
-    this._orderingID = 0
+    this._currentFileID = undefined;
+    this._currentFile = undefined;
+    this._selectedTags = [];
+    this._questions = undefined;
+    this._currentQuestion = undefined;
+    this._orderingID = 0;
 
-    this._fetchErr = undefined
+    this._fetchErr = undefined;
   }
 
   // Call when navigating to a new file
   public async establishFile(fileID: number, orderingID: number = 0): Promise<void> {
-    this.wipeVars()
+    this.wipeVars();
 
-    this._currentFileID = fileID
-    this._orderingID = orderingID
+    this._currentFileID = fileID;
+    this._orderingID = orderingID;
 
     // Get file and questions from the database
-    const { data: filedata, error: fileerr } = await this.supaService.getFileByID(this._currentFileID)
+    const { data: filedata, error: fileerr } = await this.supaService.getFileByID(this._currentFileID);
     if (fileerr) {
-      throw fileerr
+      throw fileerr;
     }
-    this._currentFile = filedata as TaggedFileEntry
+    this._currentFile = filedata as TaggedFileEntry;
     // Default selected tags to the tags on the file: the query params will overwrite this later if we have something else
-    this._selectedTags = this._currentFile.filetags.map(t => t.tagid)
+    this._selectedTags = this._currentFile.filetags.map(t => t.tagid);
 
-    const { data: questiondata, error: questionerr } = await this.supaService.listQuestions()
+    const { data: questiondata, error: questionerr } = await this.supaService.listQuestions();
     if (questionerr) {
-      throw questionerr
+      throw questionerr;
     }
-    this._questions = questiondata as QuestionWithOptions[]
+    this._questions = questiondata as QuestionWithOptions[];
 
     if (this._querySubscription) {
-      this._querySubscription.unsubscribe()
+      this._querySubscription.unsubscribe();
     }
     // Once the query parameters have loaded (we can probably shorten this further),
     // see if we're between questions and move up if we need to
     this._querySubscription = this.route.queryParams.subscribe(queryParams => {
       if (!this._questions) {
-        console.error("this._questions is undefined")
-        return
+        console.error("this._questions is undefined");
+        return;
       }
 
-      this._selectedTags = this.getNumberArrayParam(queryParams, selectedTagsParam)
+      this._selectedTags = this.getNumberArrayParam(queryParams, selectedTagsParam);
 
-      const oid: number = queryParams[orderingIDParam]
+      const oid: number = queryParams[orderingIDParam];
       if (oid) {
-        this._orderingID = oid
+        this._orderingID = oid;
       } else {
-        this._orderingID = 0 // This marks that query params have loaded, and that there was no ordering ID
+        this._orderingID = 0; // This marks that query params have loaded, and that there was no ordering ID
       }
 
       // Find next highest ordering ID
-      const nextQuestionIndex = this._questions.findIndex(q => this._orderingID != undefined && (q.orderingID ? q.orderingID : 0) >= this._orderingID)
-      const nextQuestion = nextQuestionIndex >= 0 ? this._questions[nextQuestionIndex] : undefined
+      const nextQuestionIndex = this._questions.findIndex(q => this._orderingID != undefined && (q.orderingID ? q.orderingID : 0) >= this._orderingID);
+      const nextQuestion = nextQuestionIndex >= 0 ? this._questions[nextQuestionIndex] : undefined;
       if (nextQuestion === undefined) {
         // We're done! No more questions
-        this._currentQuestion = null
-        this._completionPercentage = 100
-        return
+        this._currentQuestion = null;
+        this._completionPercentage = 100;
+        return;
       }
       if ((nextQuestion.orderingID ? nextQuestion.orderingID : 0) == this._orderingID) {
         // Our ordering ID matches! This is our current question
-        this._currentQuestion = nextQuestion
-        this._completionPercentage = (nextQuestionIndex / this._questions.length) * 100
-        return
+        this._currentQuestion = nextQuestion;
+        this._completionPercentage = (nextQuestionIndex / this._questions.length) * 100;
+        return;
       }
 
       // Navigate if not equal, we're on an ID between questions
-      this.router.navigate([`/tag`], { queryParamsHandling: 'merge', queryParams: { [orderingIDParam]: nextQuestion.orderingID, [imageParam]: this._currentFileID } })
-    })
+      this.router.navigate([`/tag`], { queryParamsHandling: 'merge', queryParams: { [orderingIDParam]: nextQuestion.orderingID, [imageParam]: this._currentFileID } });
+    });
   }
 
   public async nextQuestion(): Promise<void> {
     if (!this._questions || !this._currentFileID || !this._orderingID) {
-      return
+      return;
     }
 
-    const lastQuestion = this._questions[this._questions.length - 1]
+    const lastQuestion = this._questions[this._questions.length - 1];
 
     if (lastQuestion.orderingID && this._orderingID > lastQuestion.orderingID) {
       // If we're past the end, let's save this file
       // TODO: gracefully handle errors on tagging!
       const error = await this.supaService.patchFile(this._currentFileID, {
         hasBeenTagged: true,
-      }, this._selectedTags)
+      }, this._selectedTags);
       if (error) {
-        throw error
+        throw error;
       }
-      this.nextFile()
-      return
+      this.nextFile();
+      return;
     }
 
-    this._orderingID++
-    this._orderingID = lastQuestion.orderingID ? Math.min(lastQuestion.orderingID + 1, this._orderingID) : undefined // 1 over is okay, denotes we're done
+    this._orderingID++;
+    this._orderingID = lastQuestion.orderingID ? Math.min(lastQuestion.orderingID + 1, this._orderingID) : undefined; // 1 over is okay, denotes we're done
 
-    this.renavigate()
+    this.renavigate();
   }
 
   public previousQuestion() {
     if (!this._questions || !this._orderingID) {
       // Questions haven't loaded yet, don't do anything
-      return
+      return;
     }
 
-    const allLowerQuestions = this._questions.filter(q => this._orderingID && q.orderingID && q.orderingID < this._orderingID)
-    this._orderingID = allLowerQuestions.length > 0 ? allLowerQuestions[allLowerQuestions.length - 1].orderingID : 0
+    const allLowerQuestions = this._questions.filter(q => this._orderingID && q.orderingID && q.orderingID < this._orderingID);
+    this._orderingID = allLowerQuestions.length > 0 ? allLowerQuestions[allLowerQuestions.length - 1].orderingID : 0;
 
-    this.renavigate()
+    this.renavigate();
   }
 
   public nextFile() {
-    this.wipeVars()
-    this.wipeVars()
-    this.router.navigate(["/tag"], { queryParams: { [imageParam]: null, [orderingIDParam]: null, [selectedTagsParam]: null } })
+    this.wipeVars();
+    this.wipeVars();
+    this.router.navigate(["/tag"], { queryParams: { [imageParam]: null, [orderingIDParam]: null, [selectedTagsParam]: null } });
   }
 
   public addTag(tag: number) {
     if (!this._selectedTags.some(t => t == tag)) {
-      this._selectedTags.push(tag)
+      this._selectedTags.push(tag);
     }
-    this.renavigate()
+    this.renavigate();
   }
 
   public removeTag(tag: number) {
-    const idx = this._selectedTags.indexOf(tag)
+    const idx = this._selectedTags.indexOf(tag);
     if (idx == -1) {
-      return
+      return;
     }
-    this._selectedTags.splice(idx, 1)
-    this.renavigate()
+    this._selectedTags.splice(idx, 1);
+    this.renavigate();
+  }
+
+  public unsubscribe() {
+    this._querySubscription?.unsubscribe();
   }
 }
--- src/app/tagging/tagging.component.ts
+++ src/app/tagging/tagging.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APIUtilityService } from '../apiutility.service';
 import { QuestionManagerService } from './questionmanager.service';
@@ -11,78 +11,88 @@ import { QuestionManagerService } from './questionmanager.service';
   styleUrls: ['./tagging.component.sass']
 })
 export class TaggingComponent implements OnInit {
-  noMoreFiles: boolean = false
-  navigateError: string | undefined = undefined
+  noMoreFiles: boolean = false;
+  navigateError: string | undefined = undefined;
+
+  queryParamSub: Subscription | undefined = undefined;
 
   constructor(public router: Router, private route: ActivatedRoute, public apiUtility: APIUtilityService, public questionManager: QuestionManagerService) { }
 
   pastelColorForText(text: string | undefined): string {
     if (!text) {
-      return ""
+      return "";
     }
     const hash = text.split("").reduce((a, b) => {
       a = (a << 5) - a + b.charCodeAt(0);
       return a & a;
     }, 0);
-    const pastelStrength = '93%'
+    const pastelStrength = '93%';
     return `hsl(${hash % 360}, ${pastelStrength}, ${pastelStrength})`;
   }
 
   tagAdded(tag: number) {
-    this.questionManager.addTag(tag)
+    this.questionManager.addTag(tag);
   }
 
   tagRemoved(tag: number) {
-    this.questionManager.removeTag(tag)
+    this.questionManager.removeTag(tag);
   }
 
   nextQuestion() {
-    this.questionManager.nextQuestion()
+    this.questionManager.nextQuestion();
   }
 
   prevQuestion() {
-    this.questionManager.previousQuestion()
+    this.questionManager.previousQuestion();
   }
 
   skipImage() {
     // TODO: fix this better so we don't have a chance to redirect to the same image again
     //       Maybe add a "not" flag, so we go to any image except that one?
-    this.router.navigate(["/tag"])
+
+    // TODO: I think this is broken cuz we need to add the specific "no params" thing
+    this.router.navigate(["/tag"]);
   }
 
   toolbarNavigate(path: string) {
-    this.router.navigate([path])
+    this.unsubscribe();
+    this.router.navigate([path]);
+  }
+
+  unsubscribe() {
+    this.queryParamSub?.unsubscribe();
+    this.questionManager.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe(async params => {
-      const image = params.get("image")
+    this.queryParamSub = this.route.queryParamMap.subscribe(async params => {
+      const image = params.get("image");
 
       if (image != null && image != "") {
         // We have an image: handle it and get out!
-        const imageID = parseInt(image)
+        const imageID = parseInt(image);
 
-        this.questionManager.establishFile(imageID)
-        return
+        this.questionManager.establishFile(imageID);
+        return;
       }
 
       // Handle if we need to pick a new image
-      const { file: untaggedFile, error } = await this.apiUtility.getRandomUntaggedFile()
+      const { file: untaggedFile, error } = await this.apiUtility.getRandomUntaggedFile();
       if (error) {
         if (error instanceof HttpErrorResponse) {
-          this.navigateError = error.message
+          this.navigateError = error.message;
         } else {
-          this.navigateError = error.toString()
+          this.navigateError = error.toString();
         }
       }
 
-      this.navigateError = undefined
+      this.navigateError = undefined;
       if (untaggedFile === null) {
-        this.noMoreFiles = true
-        return
+        this.noMoreFiles = true;
+        return;
       }
 
-      this.router.navigate(["/tag"], { queryParams: { "image": untaggedFile.id } })
-    })
+      this.router.navigate(["/tag"], { queryParams: { "image": untaggedFile.id } });
+    });
   }
 }
